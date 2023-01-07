9f478de18191a65574319544d40e447ea8fe0333
Mario Montero <mario.montero@devitech.com.co>
2023-01-06 21:17:51 -0500

    initialize grapql configuration

---
+++ src/graphql/index.js
@@ -0,0 +1,28 @@
+const { ApolloServer } = require('apollo-server-express');
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require('apollo-server-core');
+
+const typeDefs = `
+  type Query {
+    hello: String
+    }
+    `;
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+ const useGraphQl = async (app) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
+
+module.exports = useGraphQl;
