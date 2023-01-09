207e099093b662a85103748000877b3856470594
Mario Montero <mario.montero@devitech.com.co>
2023-01-08 22:10:41 -0500

    adding types || load files | querys and data types in gpl

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -3,26 +3,39 @@ const {
   ApolloServerPluginLandingPageGraphQLPlayground,
 } = require('apollo-server-core');
 
-const typeDefs = `
-  type Query {
-    hello: String
-    }
-    `;
+const { loadFiles } = require('@graphql-tools/load-files')
+
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
+    hello: () => 'Hello World',
+    hello2: () => 2,
+    person: (_, args) => `Hola ${args.name} tienes ${args.age} años`,
+    getBoolean: () => true,
+    getId: () => '123456789',
+    getFloat: () => 3.1416,
+    getNumbers: (_, args) => args.numbers.map((number) => number * 2),
+    products: ()=>{
+      return {
+        id:'123',
+        name:"Cafe",
+        price:2000,
+        description:'bla bla blu',
+        image:'https://image.png',
+        createdAt: new Date().toISOString()
+      }
+    }
   },
 };
 
- const useGraphQl = async (app) => {
+const useGraphQl = async (app) => {
   const server = new ApolloServer({
-    typeDefs,
+    typeDefs: await loadFiles('./src/**/*.graphql'),
     resolvers,
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
   });
   await server.start();
   server.applyMiddleware({ app });
 };
-
 
 module.exports = useGraphQl;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const createApp = require('./app');
 const port = process.env.PORT || 3000;
 const app = createApp();
 app.listen(port, () => {
-  console.log(`Mi port ${port}`);
+  console.log(`App Node js + GraphQL running | Url: localhost:${port}/graphql`);
 });
