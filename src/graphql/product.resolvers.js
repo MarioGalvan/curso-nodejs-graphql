5aa72084af3b13c7756c8b5ebbf3d806578b4156
Mario Montero <mario.montero@devitech.com.co>
2023-01-09 11:54:08 -0500

    adding mutations | add new product with graphql

--- src/graphql/product.resolvers.js
+++ src/graphql/product.resolvers.js
@@ -1,17 +1,21 @@
-const ProductsService = require("../services/product.service");
+const ProductsService = require('../services/product.service');
 
 const productService = new ProductsService();
 
-
-const getProduct = async(_, { id }) => {
-  return await productService.findOne(id)
+const getProduct = async (_, { id }) => {
+  return await productService.findOne(id);
 };
 
-const getProducts = async() => {
-  return await productService.find({})
+const getProducts = async () => {
+  return await productService.find({});
+};
+
+const addProduct = async (_, { dto }) => {
+  return await productService.create(dto);
 };
 
 module.exports = {
   getProduct,
   getProducts,
+  addProduct,
 };
